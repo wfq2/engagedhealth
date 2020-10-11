b15436ece1ddd5ec829c69dbbcb3baa330bf498a
Wyatt Queirolo <wyatt@mavrck.co>
2020-10-11 17:03:18 -0400

    added label

--- js/read-csv.js
+++ js/read-csv.js
@@ -10,7 +10,7 @@ function updateHtml(text) {
   var parsed = Papa.parse(text, { header: true })
   parsed.data.forEach((val) => {
     if (val.First) {
-      $("#cdn-bios").append(getBio(val.First, val.Last, val.Company, val.Bio))
+      $("#cdn-bios").append(getBio(val.First, val.Last, val.Company, val.Bio, val.Label))
     }
   })
 }
@@ -20,12 +20,17 @@ const testData = `Full,First,Last,Company,Bio
 Andrew Kaplan,Andrew,Kaplan,Bain Capital,"Andrew Kaplan joined Bain Capital in 2009 as a member of the Private Equity team. He is a Principal in the Healthcare Vertical. He has been a key contributor in the acquisitions of Air Medical, Beacon Health, National Surgical Healthcare, Stada Arzneimittel, Surgery Partners, Value Options and the acquisition of assets from Healix, UPI and Bristol Myers Squibb to form the pharmaceutical services firm QuVa. Andrew currently serves on the Board of Directors of Surgery Partners, Beacon and QuVa. He was the 2016 recipient of Bain�s Excelsior Award for exceptional performance and the highest excellence in embodying the firm�s core values. Prior to Bain, Andrew was an investment banker with Goldman Sachs. He also co-founded EngagedHealth, LLC, a post-hospitalization home-based service for chronically ill, low income patients aiming to improve outcomes, reduce readmissions, and save costs. This experience has been fundamental to informing his perspective on the value delivered to patients and the broader healthcare system through higher quality, lower cost care models. Andrew received an MBA from Harvard Business School where he was a Baker Scholar. As an undergraduate, he attended The Wharton School at the University of Pennsylvania, where he served as Class President for four years, and graduated magna cum laude with a BS in Economics."`
 
 
-function getBio(first, last, company, bio) {
+function getBio(first, last, company, bio, label) {
+if (label) {
+  label = "- " + label
+} else {
+  label = ""
+}
 return `
   <div class="bgrid service-item">	
             <span class="icon"><img class="portrait" src="../images/portraits/${first}-${last}.png"></span>            
               <div class="service-content">
-                <h2 class="h05">${first} ${last}</h2>
+                <h2 class="h05">${first} ${last} ${label}</h2>
                 <h5>${company}</h5>
                 <p>${bio}
                 </p>	         		
