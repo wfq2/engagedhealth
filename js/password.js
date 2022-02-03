0b75368388f9da3dd496bc405cf1e2004d252ef4
Wyatt Queirolo <wyatt@mavrck.co>
2022-02-02 19:04:53 -0800

    updating password

--- js/password.js
+++ js/password.js
@@ -13,7 +13,7 @@ $(document).on('keypress',function(e) {
 
 function password(){
   var password = $("#pwd").val()
-  if (password == "engagedhealth2020") {
+  if (password == "engagedhealth2022") {
     window.open("main.html", "_self")
   } else {
     $(".invalidpassword").text("Invalid Password")
